bc409b6d986a7542aef6a5ba7d3d2f95fe3aa0e4
parthwebdev <parth.webdev@gmail.com>
2024-09-22 15:54:23 +0530

    refaftor word counter code to use better practices

--- 02 - Word Counter/script.js
+++ 02 - Word Counter/script.js
@@ -4,31 +4,35 @@ const characterCount = document.querySelector("[data-character-count]");
 const sentenceCount = document.querySelector("[data-sentence-count]");
 const paragraphCount = document.querySelector("[data-paragraph-count]");
 
-input.addEventListener("input", function () {
-  if (input.value) {
-    // Count Words
-    const wordsArray = input.value.split(" ").filter((word) => word !== "");
-    wordCount.innerText = wordsArray.length;
-
-    // Count Characters
-    characterCount.innerText = input.value.length;
-
-    // Count Sentences
-    const sentenceArray = input.value.split(/[.!]/);
-
-    sentenceCount.innerText = sentenceArray.length - 1;
-
-    // Count Paragraph
-    const paragraphArray = input.value
-      .split("\n")
-      .filter((p) => p.trim() !== "");
-
-    paragraphCount.innerText = paragraphArray.length;
-  } else {
-    wordCount.innerText =
-      characterCount.innerText =
-      sentenceCount.innerText =
-      paragraphCount.innerText =
-        0;
+function updateCounts() {
+  if (!input.value) {
+    resetCounts();
+    return;
   }
-});
+
+  const wordsArray = input.value.split(" ").filter((word) => word !== "");
+  wordCount.innerText = wordsArray.length;
+
+  // Count Characters
+  characterCount.innerText = input.value.length;
+
+  // Count Sentences
+  const sentenceArray = input.value.split(/[.!]/);
+
+  sentenceCount.innerText = sentenceArray.length - 1;
+
+  // Count Paragraph
+  const paragraphArray = input.value.split("\n").filter((p) => p.trim() !== "");
+
+  paragraphCount.innerText = paragraphArray.length;
+}
+
+function resetCounts() {
+  wordCount.textContent =
+    characterCount.textContent =
+    sentenceCount.textContent =
+    paragraphCount.textContent =
+      "0";
+}
+
+input.addEventListener("input", updateCounts);
